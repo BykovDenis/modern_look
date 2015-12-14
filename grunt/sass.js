a61e5c7eb54ce5948fc6b4d5c3655c482ff31120
Bykov Denis <stivenrain@gmail.com>
2015-12-14 23:20:53 +0300

    project with grunt

---
+++ grunt/sass.js
@@ -0,0 +1,30 @@
+module.exports = {
+    // Development settings
+    dev: {
+        options: {
+            outputStyle: 'nested',
+            sourceMap: true
+        },
+        files: [{
+            expand: true,
+            cwd: 'src/scss',
+            src: ['*.scss'],
+            dest: 'src/styles',
+            ext: '.css'
+        }]
+    },
+    // Production settings
+    prod: {
+        options: {
+            outputStyle: 'compressed',
+            sourceMap: false
+        },
+        files: [{
+            expand: true,
+            cwd: 'src/scss',
+            src: ['*.scss'],
+            dest: 'src/css',
+            ext: '.css'
+        }]
+    }
+};
